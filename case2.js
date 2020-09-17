8c552aeea6d96b0c97b2757338ce174a46969629
nikskuzis <nikskuzis@gmail.com>
2020-09-17 11:36:11 +0300

    case2

--- case2.js
+++ case2.js
@@ -16,23 +16,11 @@ Acceptance criteria:
 */
 const http = require('http');
 
+
+
 const server = http.createServer((req, res) =>{
-const url = req.url;
-if (url == '/'){
-    
-    res.write('<html>');
-    res.write('<head><title>Enter message></title><head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write('html');
-    return res.end();
-}
-    //process.exit;
-    res.setHeader('Content-type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My first Page</title><head>');
-    res.write('<body><h1>Hello from Node.js Server!</h1></body>');
-    
-    res.end();
+
+
 });
 
 server.listen(3005);

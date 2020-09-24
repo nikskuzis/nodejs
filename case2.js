2a88996017844df17eb596678cce78e397914895
nikskuzis <nikskuzis@gmail.com>
2020-09-24 11:00:13 +0300

    240920

--- case2.js
+++ case2.js
@@ -18,9 +18,6 @@ const http = require('http');
 const routes = require('./routes');
 
 
-const server = http.createServer((req, res) =>{
-
-
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3005);
--- routes.js
+++ routes.js
@@ -35,5 +35,4 @@ const server = http.createServer((req, res) => {
     res.end();
   }
 });
-
 server.listen(3000);
